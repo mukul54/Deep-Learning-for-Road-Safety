b4aeccee85d75884b827d1b42b247d3310d5ac0a
Mukul Ranjan <mukulranjaniitg@gmail.com>
2019-03-31 13:20:01 +0530

    Update models.py

--- Road_safety/hackathon/models.py
+++ Road_safety/hackathon/models.py
@@ -8,6 +8,7 @@ class Driver(models.Model):
     lon = models.FloatField(default=0)
     taken = models.BooleanField(default=False)
     detected = models.BooleanField(default=False)
+    drowsy = models.BooleanField(default=False)
     
     def __str__(self):
         return self.car_no
